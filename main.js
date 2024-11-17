4eca66c2e3777e1d92651133496ad4f7183e36fb
CNZW <2429671588@qq.com>
2024-11-17 14:30:24 +0800

    main file

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ window.onload = function () {
             if (bili_bv_list[bili_bv_list_m].getAttribute('data-bili-p') == null) {
                 bili_bv_list[bili_bv_list_m].setAttribute('data-bili-p', 1);
             }
-            fetch(`https://api.injahow.cn/bparse/?bv=${bili_bv_list[bili_bv_list_m].getAttribute('data-bili-bv')}&p=${bili_bv_list[bili_bv_list_m].getAttribute('data-bili-p')}&otype=json`)
+            fetch(`https://api.injahow.cn/bparse/?bv=${bili_bv_list[bili_bv_list_m].getAttribute('data-bili-bv')}&p=${bili_bv_list[bili_bv_list_m].getAttribute('data-bili-p')}&otype=json&q=80`)
                 .then(response => response.json())
                 .then(CNZW => {
                     bili_bv_url = CNZW['url'];
