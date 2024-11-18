86fbd74bb0e190f16a229efa1f387e9a9b78ba83
CNZW <2429671588@qq.com>
2024-11-18 23:44:11 +0800

    main.js - <script src="./main.js"></script>

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 window.onload = function () {
-    var bili_bv_list = document.querySelectorAll("video[data-bili-bv]"),
-        bili_bv_list_m = 0;
+    var bili_id_list = document.querySelectorAll("video[data-bili-id]"),
+        bili_id_list_m = 0;
     setbilivideourl();
     function setbilivideourl() {
-        if (bili_bv_list_m < bili_bv_list.length) {
-            if (bili_bv_list[bili_bv_list_m].getAttribute('data-bili-p') == null) {
-                bili_bv_list[bili_bv_list_m].setAttribute('data-bili-p', 1);
+        if (bili_id_list_m < bili_id_list.length) {
+            if (bili_id_list[bili_id_list_m].getAttribute('data-bili-p') == null) {
+                bili_id_list[bili_id_list_m].setAttribute('data-bili-p', 1);
             }
             fetch(`https://api.dyqvideo.com/download/url/get`, {
                 method: "POST",
@@ -13,16 +13,16 @@ window.onload = function () {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-                    "url": `https://www.bilibili.com/video/${bili_bv_list[bili_bv_list_m].getAttribute('data-bili-bv')}`,
+                    "url": `https://www.bilibili.com/video/${bili_id_list[bili_id_list_m].getAttribute('data-bili-id')}`,
                     "videoChannel": "BILI_BILI"
                 })
             })
                 .then(response => response.json())
                 .then(CNZW => {
-                    bili_bv_url = CNZW['data']['downloadUrl'];
-                    bili_bv_list[bili_bv_list_m].innerHTML = `<source src="${CNZW['data']['downloadUrl']}">`;
-                    bili_bv_list[bili_bv_list_m].setAttribute('src', CNZW['data']['downloadUrl']);
-                    bili_bv_list_m += 1;
+                    bili_id_url = CNZW['data']['downloadUrl'];
+                    bili_id_list[bili_id_list_m].innerHTML = `<source src="${CNZW['data']['downloadUrl']}">`;
+                    bili_id_list[bili_id_list_m].setAttribute('src', CNZW['data']['downloadUrl']);
+                    bili_id_list_m += 1;
                     setbilivideourl();
                 })
         }
