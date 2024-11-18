00acbc5274265b5403aa8b7b3f79505cfbf36bf8
CNZW <2429671588@qq.com>
2024-11-18 22:36:18 +0800

    main file

--- main.js
+++ main.js
@@ -7,12 +7,22 @@ window.onload = function () {
             if (bili_bv_list[bili_bv_list_m].getAttribute('data-bili-p') == null) {
                 bili_bv_list[bili_bv_list_m].setAttribute('data-bili-p', 1);
             }
-            fetch(`https://api.injahow.cn/bparse/?bv=${bili_bv_list[bili_bv_list_m].getAttribute('data-bili-bv')}&p=${bili_bv_list[bili_bv_list_m].getAttribute('data-bili-p')}&otype=json&q=80`)
+            fetch(`https://api.dyqvideo.com/download/url/get`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Origin": "https://bilibili.dyqvideo.com"
+                },
+                body: JSON.stringify({
+                    "url": `https://www.bilibili.com/video/${bili_bv_list[bili_bv_list_m].getAttribute('data-bili-bv')}`,
+                    "videoChannel": "BILI_BILI"
+                })
+            })
                 .then(response => response.json())
                 .then(CNZW => {
-                    bili_bv_url = CNZW['url'];
-                    bili_bv_list[bili_bv_list_m].innerHTML = `<source src="${CNZW['url']}">`;
-                    bili_bv_list[bili_bv_list_m].setAttribute('src', CNZW['url']);
+                    bili_bv_url = CNZW['data']['downloadUrl'];
+                    bili_bv_list[bili_bv_list_m].innerHTML = `<source src="${CNZW['data']['downloadUrl']}">`;
+                    bili_bv_list[bili_bv_list_m].setAttribute('src', CNZW['data']['downloadUrl']);
                     bili_bv_list_m += 1;
                     setbilivideourl();
                 })
